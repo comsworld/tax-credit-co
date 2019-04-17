0cf435d1e8340c26aa33618c93e70ca452269de9
Kostadin Mladenov <comsworld@gmail.com>
2019-04-16 22:05:42 -0700

    Updated remote browser driver

--- src/test/java/cucumberWebTest/stepDefinitions/frontend/RunWebTest.java
+++ src/test/java/cucumberWebTest/stepDefinitions/frontend/RunWebTest.java
@@ -3,7 +3,6 @@ package cucumberWebTest.stepDefinitions.frontend;
 import com.github.mkolisnyk.cucumber.runner.ExtendedTestNGRunner;
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import cucumber.api.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 
 @ExtendedCucumberOptions(jsonReport = "target/UI/cucumber.json",
@@ -21,7 +20,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(plugin = { "html:target/UI/cucumber-html-report",
         "json:target/UI/cucumber.json", "pretty:target/UI/cucumber-pretty.txt",
         "usage:target/UI/cucumber-usage.json", "junit:target/UI/cucumber-results.xml" },
-        features = {"C:\\Users\\Kostadin Mladenov\\Documents\\Coding\\IntelliJ Projects\\onlineStore\\src\\test\\resources\\features\\"},
+        features = {"src\\test\\resources\\features\\"},
         glue = {"cucumberWebTest.stepDefinitions"},
         tags = {"@LoginTest, @NewRegistration"})
 
